38d46e5884f6e0251fd2cdbb93a26e1c1ad0dfe8
Yaroslav Ryapolov <littlepantry@gmail.com>
2021-10-14 03:15:56 +0700

    worked on cleaning code up

--- RestManagerLogic/RestManager.cs
+++ RestManagerLogic/RestManager.cs
@@ -7,97 +7,100 @@ namespace RestManagerLogic
     public class RestManager
     {
         private readonly int _maxTableSize;
-        private readonly List<TableWithNode> _tables;
-        private readonly LinkedList<ClientsGroup> _clientsQueue = new LinkedList<ClientsGroup>();
-        
-        private readonly List<LinkedList<TableWithNode>> _tablesByAvailableChairs;
+        private readonly Dictionary<Guid, LinkedListNode<Table>> _tables;
+        private readonly List<LinkedList<Table>> _tablesBySeats;
+        private readonly LinkedList<GroupWithNode> _clientsQueue = new();
+        private readonly Dictionary<Guid, GroupWithNode> _clients = new();
 
-        public IEnumerable<Table> Tables => _tables.Select((t) => t.Table);
+        public IEnumerable<Table> Tables => _tables.Values.Select((t) => t.Value);
 
         public RestManager(List<Table> tables)
         {
-            // doing the copy to avoid effects from external list changes
-            _tables = new List<TableWithNode>(tables.Count);
+            _tables = new Dictionary<Guid, LinkedListNode<Table>>(tables.Count);
 
             _maxTableSize = tables.Max((t) => t.Size);
-            _tablesByAvailableChairs = new List<LinkedList<TableWithNode>>(_maxTableSize + 1);
-            for (int i = 0; i < _tablesByAvailableChairs.Capacity; i++)
+            _tablesBySeats = new List<LinkedList<Table>>(_maxTableSize + 1);
+            for (int i = 0; i < _tablesBySeats.Capacity; i++)
             {
-                _tablesByAvailableChairs.Add(new LinkedList<TableWithNode>());
+                _tablesBySeats.Add(new LinkedList<Table>());
             }
 
             foreach (var table in tables)
             {
-                var tableWithNode = new TableWithNode(table);
-                tableWithNode.Node = _tablesByAvailableChairs[table.AvailableChairs].AddLast(tableWithNode);
-                
-                _tables.Add(tableWithNode);
+                var tableNode = _tablesBySeats[table.AvailableChairs].AddLast(table);
+                _tables.Add(table.Guid, tableNode);
             }
         }
 
         public void OnArrive(ClientsGroup group)
         {
             // TO LOCK Queue
-            _clientsQueue.AddLast(group);
-            
-            this.TryToSeatSomebodyFromQueue();
+            var groupWithNode = new GroupWithNode(group);
+            groupWithNode.Node = _clientsQueue.AddLast(groupWithNode);
+
+            _clients[group.Guid] = groupWithNode;
+
+            TrySeatSomebodyFromQueue();
         }
 
         public void OnLeave(ClientsGroup group)
         {
-            // TO LOCK Queue
-            if (_clientsQueue.Remove(group))
+            try
             {
-                return;
-            }
+                // TO LOCK Queue
+                var groupWithNode = _clients[group.Guid];
+                if (groupWithNode == null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(group), "Group is not found");
+                }
 
-            // TO LOCK Tables Matrix
-            var table = this.Lookup(group);
-            if (table == null)
+                if (groupWithNode.Node != null)
+                {
+                    _clientsQueue.Remove(groupWithNode.Node);
+                    return;
+                }
+
+                // TO LOCK Tables Matrix
+                var table = Lookup(group);
+                if (table == null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(group), "Group is not found at any table");
+                }
+
+                var tableNode = _tables[table.Guid];
+
+                _tablesBySeats[table.AvailableChairs].Remove(tableNode);
+                var newTableHead = _tablesBySeats[table.AvailableChairs + group.Size];
+                if (table.Size == table.AvailableChairs + group.Size)
+                {
+                    // put free tables as first possible options at the beginning
+                    newTableHead.AddFirst(tableNode);
+                }
+                else
+                {
+                    // put occupied tables at the end in case there is no free tables
+                    newTableHead.AddLast(tableNode);
+                }
+
+                table.ReleaseChairs(group);
+
+                TrySeatSomebodyFromQueue();
+            }
+            finally
             {
-                throw new ArgumentOutOfRangeException(nameof(group), "Group is not found at any table");
+                _clients.Remove(group.Guid);
             }
-
-            var tableWithNode = _tables.Single((t) => t.Table == table);
-            
-            // todo: receive LinkedList and use it to remove faster
-            _tablesByAvailableChairs[table.AvailableChairs].Remove(tableWithNode);
-            var newTableHead = _tablesByAvailableChairs[table.AvailableChairs + group.Size];
-            if (table.Size == table.AvailableChairs + group.Size)
-            {
-                // put free tables as first possible options at the beginning
-                newTableHead.AddFirst(tableWithNode);
-            }
-            else
-            {
-                // put occupied tables at the end in case there is no free tables
-                newTableHead.AddLast(tableWithNode);
-            }
-
-            table.ReleaseChairs(group);
-
-            this.TryToSeatSomebodyFromQueue();
         }
         
         public Table Lookup(ClientsGroup group)
         {
             // TO LOCK Tables Matrix
-            
-            // нужно причесать чуток и оптимизировать (хешиком, где хранится линкед лист на стол, который также обновляется в случае необходимости)
-            return _tables
-                .SelectMany((t) => t.Table.SeatedClientGroups
-                    .Select((g) => new
-                    {
-                        table = t.Table,
-                        group = g
-                    })
-                )
-                .Where((t) => t.group == group)
-                .Select((t) => t.table)
-                .SingleOrDefault();
+            // TO LOCK Queue
+
+            return _clients[group.Guid].TableNode?.Value;
         }
 
-        public void TryToSeatSomebodyFromQueue()
+        public void TrySeatSomebodyFromQueue()
         {
             // TO LOCK Queue
             int minimumNotSeatedSize = _maxTableSize + 1;
@@ -109,62 +112,89 @@ namespace RestManagerLogic
                 queueItem = queueItem.Next;
 
                 
-                if (current.Value.Size >= minimumNotSeatedSize)
+                if (current.Value.Group.Size >= minimumNotSeatedSize)
                 {
                     continue;
                 }
 
-                if (this.TrySeatClientsGroup(current.Value))
+                if (TrySeatClientsGroup(current.Value.Group))
                 {
                     _clientsQueue.Remove(current);
+                    current.Value.Node = null;
                 }
                 else
                 {
-                    minimumNotSeatedSize = current.Value.Size;
+                    minimumNotSeatedSize = current.Value.Group.Size;
                 }
             }
         }
 
-        public bool TrySeatClientsGroup(ClientsGroup group)
+        private bool TrySeatClientsGroup(ClientsGroup group)
         {
             // TO LOCK Tables Matrix
             
-            var tablesWithEnoughRoom = _tablesByAvailableChairs[group.Size];
+            var enoughRoomTables = _tablesBySeats[group.Size];
             int i = group.Size + 1;
-            while ((!tablesWithEnoughRoom.Any() || tablesWithEnoughRoom.First?.Value.Table.IsOccupied == true)
-                   && i < _tablesByAvailableChairs.Count)
+            while (IsEmptyOrAllOccupied(enoughRoomTables) && i < _tablesBySeats.Count)
             {
-                if (tablesWithEnoughRoom.First?.Value.Table.IsOccupied != true
-                    || _tablesByAvailableChairs[i].First?.Value.Table.IsOccupied == false)
+                if (HasFreeTable(_tablesBySeats[i]))
                 {
-                    tablesWithEnoughRoom = _tablesByAvailableChairs[i];
+                    enoughRoomTables = _tablesBySeats[i];
                 }
 
                 i++;
             }
 
-            var table = tablesWithEnoughRoom.First?.Value;
-            if (table == null)
+            var tableNode = enoughRoomTables.First;
+            if (tableNode == null)
             {
                 return false;
             }
 
             // move table to new list
-            tablesWithEnoughRoom.RemoveFirst();
-            _tablesByAvailableChairs[table.Table.AvailableChairs - group.Size].AddLast(table);
+            enoughRoomTables.RemoveFirst();
+            _tablesBySeats[tableNode.Value.AvailableChairs - group.Size].AddLast(tableNode);
 
-            table.Table.SeatClientsGroup(group);
+            // assign group to table
+            var groupWithNode = _clients[group.Guid];
+            groupWithNode.TableNode = tableNode;
+            
+            tableNode.Value.SeatClientsGroup(group);
             return true;
         }
-        
-        private class TableWithNode
-        {
-            public readonly Table Table;
-            public LinkedListNode<TableWithNode> Node;
 
-            public TableWithNode(Table table)
+        private bool HasFreeTable(LinkedList<Table> tables)
+        {
+            var first = tables.First;
+            if (first == null)
             {
-                this.Table = table;
+                return false;
+            }
+
+            return !first.Value.IsOccupied;
+        }
+
+        private bool IsEmptyOrAllOccupied(LinkedList<Table> tables)
+        {
+            var first = tables.First;
+            if (first == null)
+            {
+                return true;
+            }
+
+            return first.Value.IsOccupied;
+        }
+
+        // TODO: Есть смысл переименовать в GroupInRest
+        private class GroupWithNode
+        {
+            public readonly ClientsGroup Group;
+            public LinkedListNode<Table> TableNode;
+            public LinkedListNode<GroupWithNode> Node;
+
+            public GroupWithNode(ClientsGroup group)
+            {
+                Group = group;
             }
         }
     }
