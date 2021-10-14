0549f8eb19a434d1d0efe7b7d03e171f1b0efbf6
Yaroslav Ryapolov <littlepantry@gmail.com>
2021-10-14 21:18:07 +0700

    started adding some MVC elements to have visual controls for the task

---
+++ RestManagerWeb/Models/ClientsGroupViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace RestManagerWeb.Models
+{
+    public class ClientsGroupViewModel
+    {
+        public readonly Guid Guid;
+        public readonly int Size;
+    }
+}
---
+++ RestManagerWeb/Models/TableViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace RestManagerWeb.Models
+{
+    public class TableViewModel
+    {
+        public readonly Guid Guid;
+        public readonly int Size;
+
+        public readonly List<ClientsGroupViewModel> _seatedClientsGroups = new();
+
+        public int AvailableChairs;
+        public bool IsOccupied;
+    }
+}
